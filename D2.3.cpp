40c68eb151154a8e3e218eb42b17063c74262767
tolokol <blackjamgo@gmail.com>
2023-01-18 18:55:08 +0300

    my2.3

---
+++ D2.3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+    int num1, num2;
+    int pass1 = 1234;
+    int pass2 = 5678;
+
+    std::cout << "Enter first number: ";
+    std::cin >> num1;
+    std::cout << "Enter second number: ";
+    std::cin >> num2;
+
+    if (num1 == pass1 && num2 == pass2) {
+        std::cout << "OK" << std::endl;
+    } else {
+        std::cout << "error" << std::endl;
+    }
+
+    return 0;
+}
