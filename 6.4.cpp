d4722c3e814a4626f4c210e11bf66c9ae117204f
tolokol <blackjamgo@gmail.com>
2023-01-22 14:34:28 +0300

    6.4 nw

--- 6.4.cpp
+++ 6.4.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 int main() {
     char str[100];
-    cout << "Enter a string: ";
+    cout << "Введите строку: ";
     cin >> str;
 
     for (int i = 0; str[i] != '\0'; i++) {
         str[i] = str[i + 1];
     }
 
-    cout << "String after removing the first character: " << str << endl;
+    cout << "Строка после удаления символа: " << str << endl;
 
     return 0;
 }
