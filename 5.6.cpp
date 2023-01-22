063d57337c0db493bcfcbfd1af9e2ee6f38d6e85
tolokol <blackjamgo@gmail.com>
2023-01-22 13:28:02 +0300

    5.6 new

--- 5.6.cpp
+++ 5.6.cpp
@@ -3,31 +3,42 @@
 using namespace std;
 
 int main() {
-    int n, m;
-    cout << "Введите количество строк и столбцов массива: ";
-    cin >> n >> m;
+int n, m;
+cout << "Введите количество строк и столбцов массива: ";
+cin >> n >> m;
 
-    int arr[n][m];
+int arr[n][m];
 
-    cout << "Введите элементы массива: " << endl;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            cin >> arr[i][j];
-        }
+cout << "Введите элементы массива: " << endl;
+for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+        cin >> arr[i][j];
     }
+}
 
-    
-    for (int i = 0; i < n; i++) {
-        sort(arr[i], arr[i] + m);
+
+for (int i = 0; i < n; i++) {
+    sort(arr[i], arr[i] + m); 
+}
+
+cout << "Отсортированный массив: " << endl;
+for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+        cout << arr[i][j] << " ";
     }
+    cout << endl;
+}
 
-    cout << "Отсортированный массив : " << endl;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            cout << arr[i][j] << " ";
-        }
-        cout << endl;
+for(int i = 0; i < n; i++) {
+    swap(arr[i][0], arr[i][m-1]);
+}
+cout << "Массив после замены первого и последнего столбцов: " << endl;
+for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+        cout << arr[i][j] << " ";
     }
+    cout << endl;
+}
+return 0;
 
-    return 0;
 }
